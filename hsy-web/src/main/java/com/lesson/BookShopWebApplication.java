9ad337b4b6398729224d13f2318632824e01fe31
UPIC <2487718845@QQ.com>
2017-12-08 10:23:20 +0800

    小更新

--- hsy-web/src/main/java/com/lesson/BookShopWebApplication.java
+++ hsy-web/src/main/java/com/lesson/BookShopWebApplication.java
@@ -5,19 +5,13 @@ package com.lesson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author zhailiang
  */
 @SpringBootApplication
-public class BookShopWebApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(BookShopWebApplication.class);
-    }
-
+public class BookShopWebApplication {
+    
     /**
      * @param args
      */
